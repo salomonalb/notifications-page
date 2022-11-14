718b0a40d47ac05f59b19ad1e8ae700f85bea24d
salomonalb <95834451+salomonalb@users.noreply.github.com>
2022-11-14 17:05:42 -0400

    images module done

--- src/ts/images.ts
+++ src/ts/images.ts
@@ -1,4 +1,11 @@
-import avatarAngela from '../assets/images/avatar-angela-gray.webp'
+import avatarAngela from '../assets/images/avatar-angela-gray.webp';
+import avatarAnna from '../assets/images/avatar-anna-kim.webp';
+import avatarJacob from '../assets/images/avatar-jacob-thompson.webp';
+import avatarKimberly from '../assets/images/avatar-kimberly-smith.webp';
+import avatarMark from '../assets/images/avatar-mark-webber.webp';
+import avatarNathan from '../assets/images/avatar-nathan-peterson.webp';
+import avatarRizky from '../assets/images/avatar-rizky-hasanuddin.webp';
+import imgChess from '../assets/images/image-chess.webp';
 
 
-export default avatarAngela;
+export { avatarAngela, avatarAnna, avatarJacob, avatarKimberly, avatarMark, avatarNathan, avatarRizky, imgChess }
