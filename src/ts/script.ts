11996243a104c957ed18660aadf9120815533eeb
salomonalb <95834451+salomonalb@users.noreply.github.com>
2022-11-15 16:32:50 -0400

    import read function

--- src/ts/script.ts
+++ src/ts/script.ts
@@ -1,6 +1,8 @@
 import notifications from "./notifications";
 import renderNotifs from "./render-notifs";
 import counter from "./counter";
+import readAll from "./read-all";
 
 renderNotifs(notifications);
 counter()
+readAll()
